94a83c84ecf637559154b954fe0dbdfe42dbe040
032-Abhishek Choksi <19bmiit032@gmail.com>
2022-02-15 19:44:30 +0530

    Registration and Login Page Design completed @ 2022 #032AbhishekChoksi. All rights reserved.

--- app/src/main/java/com/abhishekchoksi/orderfoodv1/LoginActivity.java
+++ app/src/main/java/com/abhishekchoksi/orderfoodv1/LoginActivity.java
@@ -2,7 +2,9 @@ package com.abhishekchoksi.orderfoodv1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    public void signin(View view){
+        startActivity(new Intent(LoginActivity.this,MainActivity.class));
+    }
+    public void signup(View view){
+        startActivity(new Intent(LoginActivity.this,RegistrationActivity.class));
     }
 }
--- app/src/main/java/com/abhishekchoksi/orderfoodv1/RegistrationActivity.java
+++ app/src/main/java/com/abhishekchoksi/orderfoodv1/RegistrationActivity.java
@@ -2,7 +2,9 @@ package com.abhishekchoksi.orderfoodv1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+    }
+
+    public void signup(View view){
+        startActivity(new Intent(RegistrationActivity.this,MainActivity.class));
+    }
+    public void signin(View view){
+        startActivity(new Intent(RegistrationActivity.this,LoginActivity.class));
     }
 }
