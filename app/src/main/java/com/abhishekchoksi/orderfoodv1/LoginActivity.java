6d069d9a12e4abe43489b04bd404564b96553fa8
032-Abhishek Choksi <19bmiit032@gmail.com>
2022-02-16 18:26:13 +0530

    Added Registration and Login Functionality @ 2022 #032AbhishekChoksi. All rights reserved.

--- app/src/main/java/com/abhishekchoksi/orderfoodv1/LoginActivity.java
+++ app/src/main/java/com/abhishekchoksi/orderfoodv1/LoginActivity.java
@@ -1,22 +1,91 @@
 package com.abhishekchoksi.orderfoodv1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private TextView textViewSignUp;
+    private EditText email,password;
+    private Button btnSignIn;
+    private ProgressBar loadingPB;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+
+        textViewSignUp = findViewById(R.id.textViewSignUp);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        btnSignIn = findViewById(R.id.btnSignIn);
+        loadingPB = findViewById(R.id.idPBLoadingSignIn);
+        mAuth = FirebaseAuth.getInstance();
+
+        // adding click listener for Sign In button.
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // checking if the password and confirm password is equal or not.
+                loadingPB.setVisibility(View.VISIBLE);
+
+                // getting data from our edit text.
+                String UserEmail = email.getText().toString();
+                String UserPassword = password.getText().toString();
+
+                // checking if the text fields are empty or not.
+                if(TextUtils.isEmpty(UserEmail) || TextUtils.isEmpty(UserPassword)){
+                    Toast.makeText(LoginActivity.this, "Please add your credentials..", Toast.LENGTH_SHORT).show();
+                } else if(UserPassword.length() < 6){
+                    Toast.makeText(LoginActivity.this, "Password Too Short, Please Enter Minimum 6 Characters!", Toast.LENGTH_SHORT).show();
+                } else{
+                    // on below line we are creating a new user by passing email and password.
+                    mAuth.signInWithEmailAndPassword(UserEmail,UserPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            // on below line we are checking if the task is success or not.
+                            if(task.isSuccessful()){
+                                // in on success method we are hiding our progress bar and opening a Sign In activity.
+                                loadingPB.setVisibility(View.GONE);
+                                Toast.makeText(LoginActivity.this, "Login Successful..", Toast.LENGTH_SHORT).show();
+
+                                // on below line we are opening our mainactivity.
+                                startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                                finish();
+                            } else {
+                                // in else condition we are displaying a failure toast message.
+                                loadingPB.setVisibility(View.GONE);
+                                Toast.makeText(LoginActivity.this, "Please Enter Valid User Credentials..!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+        // adding on click for Sign Up textView.
+        textViewSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,RegistrationActivity.class));
+                finish();
+            }
+        });
     }
-    public void signin(View view){
-        startActivity(new Intent(LoginActivity.this,MainActivity.class));
-    }
-    public void signup(View view){
-        startActivity(new Intent(LoginActivity.this,RegistrationActivity.class));
-    }
+
 }
--- app/src/main/java/com/abhishekchoksi/orderfoodv1/RegistrationActivity.java
+++ app/src/main/java/com/abhishekchoksi/orderfoodv1/RegistrationActivity.java
@@ -1,23 +1,88 @@
 package com.abhishekchoksi.orderfoodv1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
-
+    private TextView textViewSignIn;
+    private EditText email,password;
+    private Button btnSignUp;
+    private ProgressBar loadingPB;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-    }
+        getSupportActionBar().hide();
 
-    public void signup(View view){
-        startActivity(new Intent(RegistrationActivity.this,MainActivity.class));
-    }
-    public void signin(View view){
-        startActivity(new Intent(RegistrationActivity.this,LoginActivity.class));
+        textViewSignIn = findViewById(R.id.textViewSignIn);
+        email = findViewById(R.id.EditTextEmail);
+        password = findViewById(R.id.EditTextPassword);
+        btnSignUp = findViewById(R.id.btnSignUp);
+        loadingPB = findViewById(R.id.idPBLoading);
+        mAuth = FirebaseAuth.getInstance();
+
+        // adding click listener for Sign Up button.
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // checking if the password and confirm password is equal or not.
+                loadingPB.setVisibility(View.VISIBLE);
+
+                // getting data from our edit text.
+                String UserEmail = email.getText().toString();
+                String UserPassword = password.getText().toString();
+
+                // checking if the text fields are empty or not.
+                if(TextUtils.isEmpty(UserEmail) || TextUtils.isEmpty(UserPassword)){
+                    Toast.makeText(RegistrationActivity.this, "Please add your credentials..", Toast.LENGTH_SHORT).show();
+                }else if(UserPassword.length() < 6){
+                    Toast.makeText(RegistrationActivity.this, "Password Too Short, Please Enter Minimum 6 Characters!", Toast.LENGTH_SHORT).show();
+                } else{
+                    // on below line we are creating a new user by passing email and password.
+                    mAuth.createUserWithEmailAndPassword(UserEmail,UserPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            // on below line we are checking if the task is success or not.
+                            if(task.isSuccessful()){
+                                // in on success method we are hiding our progress bar and opening a Sign In activity.
+                                loadingPB.setVisibility(View.GONE);
+                                Toast.makeText(RegistrationActivity.this, "User Registered..", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(RegistrationActivity.this,LoginActivity.class));
+                                finish();
+                            } else {
+                                // in else condition we are displaying a failure toast message.
+                                loadingPB.setVisibility(View.GONE);
+                                Toast.makeText(RegistrationActivity.this, "Fail to register user..\n" + task.getException().toString(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+        // adding on click for Sign In textView.
+        textViewSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegistrationActivity.this,LoginActivity.class));
+                finish();
+            }
+        });
     }
 }
